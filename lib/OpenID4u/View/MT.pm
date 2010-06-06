0f5b2b898009441e8f13c69678f3e03de264a9bd
Nobuo Danjou <nobuo.danjou@gmail.com>
2010-06-06 23:17:56 +0900

    stop force overwrite content_type

--- lib/OpenID4u/View/MT.pm
+++ lib/OpenID4u/View/MT.pm
@@ -3,7 +3,9 @@ use Ark 'View::MT';
 
 after 'process' => sub {
     my ($self, $c) = @_;
-    $c->res->content_type('text/html; charset=utf-8');
+    unless ($c->res->content_type) {
+        $c->res->content_type('text/html; charset=utf-8');
+    }
 };
 
 1;
